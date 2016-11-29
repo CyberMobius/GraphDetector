7acdfd64252d3300910267f206c3e932915104ef
Brian Mata <bzm55@psu.edu>
2016-11-28 21:02:39 -0500

    Properly split pictures into YCbCr channels
    Also added empty method to find percent simlarity

--- Image/Source.cpp
+++ Image/Source.cpp
@@ -17,7 +17,9 @@ int main(int argc, char** argv)
 	Mat compareImage[3];
 	Mat wholeFrame, compare;
 
-	split(imread("c:/users/brimy/desktop/resources/Code/Personal Projects/Resources/human.jpg"), compareImage );
+	compare = imread("c:/users/brimy/desktop/resources/Code/Personal Projects/Resources/human.jpg");
+	cvtColor(compare, compare,CV_RGB2YCrCb);
+	split(compare, compareImage);
 
 	for (int i = 0; i < 3; i ++) {
 		namedWindow(to_string(i),WINDOW_AUTOSIZE);
@@ -32,12 +34,13 @@ int main(int argc, char** argv)
 
 	
 	while (true) {
-		waitKey(1);
+		if(waitKey(1)>0) break;
 	}
 
 	while (true) {
 		
 		cap >> wholeFrame;
+		cvtColor(wholeFrame, wholeFrame, CV_RGB2YCrCb);
 		split(wholeFrame, frame);
 		
 		
@@ -47,10 +50,21 @@ int main(int argc, char** argv)
 			imshow(to_string(i), frame[i]);
 		}
 		
+		double sum = 0;
+		for (int i = 0; i < 3; i++) {
+			sum = sum + comparison(compareImage[i], frame[i]);
+		}
+		cout << sum;
+
 		if (waitKey(1) >= 0) break;
 	}
 	
 	//waitKey(0);
+
+	return 0;
+}
+
+double comparison(Mat reference, Mat video) {
 
 	return 0;
 }
