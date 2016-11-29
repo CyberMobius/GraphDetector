2414526524a39a991d6b7862f08903fb6fcb7112
Brian Mata <bzm55@psu.edu>
2016-11-28 20:34:35 -0500

    Seperated Channels
    Added split as well as a reference image with which to compare

--- Image/Source.cpp
+++ Image/Source.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
+#include <string>
 
 using namespace cv;
 using namespace std;
@@ -10,37 +11,45 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	
-	system("color a");
+	int r, g, b;
 
-	Mat frame;
+	Mat frame[3]; 
+	Mat compareImage[3];
+	Mat wholeFrame, compare;
+
+	split(imread("c:/users/brimy/desktop/resources/Code/Personal Projects/Resources/human.jpg"), compareImage );
+
+	for (int i = 0; i < 3; i ++) {
+		namedWindow(to_string(i),WINDOW_AUTOSIZE);
+		imshow(to_string(i),compareImage[i]);
+	}
+
 
 	VideoCapture cap(0);
 	if (!cap.isOpened()) {
 		return -1;
 	}
 
-	namedWindow("Video",WINDOW_AUTOSIZE);
-	//namedWindow("Grey Video", WINDOW_AUTOSIZE);
-	//namedWindow("Raw Video", WINDOW_AUTOSIZE);
+	
+	while (true) {
+		waitKey(1);
+	}
 
 	while (true) {
 		
-		cap >> frame;
-		//imshow("Raw Video", frame);
-
-
-		cvtColor(frame,frame,CV_BGR2GRAY);
-		//imshow("Grey Video", frame);
-
-
-		GaussianBlur(frame,frame,Size(3,3),0,0);
+		cap >> wholeFrame;
+		split(wholeFrame, frame);
 		
-		Canny(frame,frame,600,1500,5,false);   
-		imshow("Video", frame);
+		
+
+		for (int i = 0; i < 3; i++) {
+			namedWindow(to_string(i), WINDOW_AUTOSIZE);
+			imshow(to_string(i), frame[i]);
+		}
 		
 		if (waitKey(1) >= 0) break;
 	}
-
+	
 	//waitKey(0);
 
 	return 0;
