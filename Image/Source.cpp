990183eaf170cce1f61d9064e7db799a9d68f3cb
Brian Mata <bzm55@psu.edu>
2016-11-30 23:30:47 -0500

    Added Full Detection of Graph Objects

--- Image/Source.cpp
+++ Image/Source.cpp
@@ -12,32 +12,43 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	namedWindow("Picture", WINDOW_AUTOSIZE);
+	namedWindow("Lines", WINDOW_AUTOSIZE);
 
-	Mat frame = imread("C:/Users/brimy/OneDrive/Pictures/Hamiltonex.jpg");
+	Mat frame = imread("C:/Users/brimy/OneDrive/Pictures/graph.png");
 	imshow("Picture", frame);
 	
 
 	vector<Vec3f> nodes;
-
-	
+	vector<Vec4i> u;
 
 	cvtColor(frame, frame, CV_RGB2GRAY);
+	
+	Mat edges(frame.size(), CV_8U);
 
-	HoughCircles(frame, nodes, CV_HOUGH_GRADIENT, 2, 100);
+	Canny(frame, edges, 300, 800, 5);
+
+	HoughCircles(frame, nodes, CV_HOUGH_GRADIENT, 2, 150);
 
 	Mat verticies(frame.size(), CV_8U);
 
 	for (size_t i = 0; i < nodes.size(); i++) {
 		Point center(cvRound(nodes[i][0]), cvRound(nodes[i][1]));
-		int radius = cvRound(nodes[i][2]);
+		nodes[i][2] = 1.25*cvRound(nodes[i][2]);
+		int radius = nodes[i][2];
 
-		circle(verticies, center, radius, Scalar(255,255,255), 4);
-
+		circle(verticies, center, radius, Scalar(0,0,255), 4);
 	}
 
-	//Canny(frame,edges,300,800,5);
-	imshow("Picture", verticies);
+	HoughLinesP(edges, u, 1, CV_PI / 180, 50,50,10);
+	Mat lines(frame.size(), CV_8U);
 
+	for (size_t i = 0; i < u.size(); i++) {
+		Vec4i l = u[i];
+		line(verticies, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+		
+	}
+	//imshow("Lines",lines);
+	imshow("Picture", verticies);
 
 	waitKey(0);
 
