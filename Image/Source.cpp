44d919b45871875ed9a828eb1f5091371a28c51c
Brian Mata <bzm55@psu.edu>
2016-12-05 14:31:07 -0500

    Debuged and commented PointInQuad

--- Image/Source.cpp
+++ Image/Source.cpp
@@ -84,28 +84,100 @@ void process(String path = "../graph.png") {
 }
 
 bool insideQuad(Point corners[4],Point test) {
+	/*
+	
+	Assuming that the quadilateral is convex, determining 
+	whether or not a point lies within it can be determined
+	by the following algorithm:
+
+	
+	Draw a line from each of the 4 corners to the test point
+	
+	At this point there are 4 triangles
+
+	Find the area of each of the 4 triangles and add them up.
+		On a side note, there are two practical ways to find this area, Heron's formula
+		or ABsin(c)/2 where sin(c) can be easily found using the dot product 
+		definition
+
+	If: the area of all 4 triangles adds up to the area of the quadilateral
+		Then: the test point lies within the quadilateral
+	Else: The point is outside the quadilateral
+
+	NOTE: ONLY WORKS FOR CONVEX QUADILATERALS
+	Extends to any convex polygon
+	Divide figure figure into triangles for concave polygons 
+	*/
+
+	//Holds the lengths of every point-corner pair
 	double lengths[4];
 
+	//Assigns lengths[i] to the distance from corners[i] to the test point
 	for (int i = 0; i < 4; i ++) {
 		
 		lengths[i] = dist(corners[i],test);
 	}
 
+
+	//Holds the sum of all of the areas of the side-test triangles
 	double area = 0;
+
+	//Takes area and adds onto it the area of each individual triangle
 	for (int i = 0; i < 4; i ++) {
-		area = area + heron(lengths[i], lengths[(i+1)%4],dist(corners[i],corners[(i+1)%4]));
+
+		//Heron's formula on lengths of each side of the triangle
+		double sides[3] = { lengths[i], lengths[(i + 1) % 4],dist(corners[i],corners[(i + 1) % 4]) };
+		
+		//Adds on additional area
+		area = area + heron(sides);
+	}
+
+	/*
+
+	The area of a quadilateral can be found by dividing it into two triangles
+	and adding the area of the two triangles
+
+	*/
+
+
+	//Holds the side lengths of the two triangles the quadilateral will be split into
+	double quadLengths[2][3];
+	
+	//{{0,1},{1,2},{2,0}}
+	//Use %3
+	//{{0, 1 % 3},{1, 2 % 3},{2, 3 % 3}}
+	for (int i = 0; i < 2; i++) {
+
+		//Assigns quadLengths[i] to the distance between corners[i] and corners[i + 1]
+		quadLengths[0][i] = dist(corners[i], corners[(i + 1)%3]);
+	}
+
+	//{{0,3},{3,2},{2,0}}
+	//Use %4
+	//{{4 % 4, 3 % 4},{3%4,2%4},{2%4,1%4}}
+	//Last entry gives {2,1} instead of desired{2,0} so reassign
+	for(int i = 4; i > 2; i--){
+
+		//Array will write bacwards 2->1 but heron's formula isn't sensitive to order
+		quadLengths[1][i-2] = dist(corners[i%4],corners[(i-1)%4]);
+	}
+	quadLengths[1][0] = dist(corners[2], corners[0]);
+
+	//Compares the area of the quadilateral to the area of the triangles
+	//Using sqrt leaves room for floating point and round error so use small epsilon
+	if (abs(heron(quadLengths[1]) + heron(quadLengths[0]) - area) < .00001) {
+		return true;
+	}
+	else {
+		return false;
 	}
 
 
-
-
-	
 }
 
-double heron(double a, double b, double c) {
+double heron(double sides[3]) {
+	//"https://en.wikipedia.org/wiki/Heron's_formula"
 	double semisum = 0;
-	
-	double sides[3] = { a,b,c };
 
 	for (int i = 1; i < 3;i++) {
 		semisum = semisum + sides[i] / 2;
@@ -122,6 +194,7 @@ double heron(double a, double b, double c) {
 }
 
 double dist(Point a, Point b) {
+	//https://en.wikipedia.org/wiki/Pythagorean_theorem
 	double x = pow(double(a.x - b.x), 2);
 	double y = pow(double(a.y - b.y), 2);
 	return sqrt(x+y);
